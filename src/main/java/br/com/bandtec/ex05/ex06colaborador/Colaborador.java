3195368acf17aa5007530126e12258fb5ebd3a2d
Lucas Mesquita <lucas.msouza@bandtec.com.br>
2021-09-25 19:13:00 -0300

    exercíicio 05 recursos humanos

---
+++ src/main/java/br/com/bandtec/ex05/ex06colaborador/Colaborador.java
@@ -0,0 +1,66 @@
+package br.com.bandtec.ex05.ex06colaborador;
+
+public class Colaborador {
+    
+    public Colaborador(String nome, String cargo, Double salario) {
+        this.nome = nome;
+        this.cargo = cargo;
+        this.salario = salario;
+    }
+
+    public Colaborador(String nome, String cargo, Integer salario) {
+        this(nome, cargo, Double.valueOf(salario));
+    }
+
+    private String nome;
+    private String cargo;
+    private Double salario;
+
+    /**
+     * @return nome do Colaborador
+     */
+    public String getNome() {
+        return this.nome;
+    } 
+
+    /**
+     * @return cargo do Colaborador
+     */
+    public String getCargo() {
+        return this.cargo;
+    }
+
+    /**
+     * atribui o valor de @param novoCargo 
+     * como o cargo de Colaborador
+     */
+    public void setCargo(String novoCargo) {
+        this.cargo = novoCargo;
+    }
+
+    /**
+     * @return salário do Colaborador
+     */
+    public Double getSalario() {
+        return this.salario;
+    }
+
+    /**
+     * atribui o valor de @param novoSalario ao
+     * salário do Colaborador
+     */
+    public void setSalario(Double novoSalario) {
+        this.salario = novoSalario;
+    }
+    // overloading
+    public void setSalario(Integer novoSalario) {
+        setSalario(Double.valueOf(novoSalario));
+    }
+
+    @Override
+    public String toString() {
+        String str = "\nNome: %s\nCargo: %s\nSalario: R$ %.2f";
+        return String.format(str, getNome(), getCargo(), getSalario());
+    } 
+
+}
