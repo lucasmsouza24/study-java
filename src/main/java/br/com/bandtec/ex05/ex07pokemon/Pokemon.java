df9b46c1dc61e22c859a1de6c2c45d7a4404cc1e
Lucas Mesquita <lucas.msouza@bandtec.com.br>
2021-09-25 21:57:45 -0300

    exercicio 07 pokemon

---
+++ src/main/java/br/com/bandtec/ex05/ex07pokemon/Pokemon.java
@@ -0,0 +1,65 @@
+package br.com.bandtec.ex05.ex07pokemon;
+
+public class Pokemon {
+    
+    public Pokemon(String nome, String tipo, Double forca) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.forca = forca;
+        this.doces = 0;
+    }
+    public Pokemon(String nome, String tipo, Integer forca) {
+        this(nome, tipo, Double.valueOf(forca));
+    }
+    
+    // atributos
+    private String nome;
+    private String tipo;
+    private Double forca;
+    private Integer doces;
+
+    // nome
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // tipo
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    // forca
+    public Double getForca() {
+        return forca;
+    }
+    public void setForca(Double forca) {
+        this.forca = forca;
+    }
+    public void setForca(Integer forca) {
+        setForca(Double.valueOf(forca));
+    }
+
+    // doces
+    public Integer getDoces() {
+        return doces;
+    }
+    
+    public void setDoces(Integer doces) {
+        this.doces = doces;
+    }
+
+    public void evoluir() {
+        // 
+    }
+
+    @Override
+    public String toString() {
+        return String.format("\nPokemon\nNome: %s\nTipo: %s\nForca: %.1f\nDoces: %d", this.nome, this.tipo, this.forca, this.doces);
+    }
+}
---
+++ src/main/java/br/com/bandtec/ex05/ex07pokemon/TestePokemon.java
@@ -0,0 +1,35 @@
+package br.com.bandtec.ex05.ex07pokemon;
+
+public class TestePokemon {
+    
+    public static void main(String[] args) {
+
+        TreinadorPokemon tp = new TreinadorPokemon("Ash");
+
+        Pokemon p1 = new Pokemon("Pikachu", "raio", 500);
+        Pokemon p2 = new Pokemon("Squirtle", "agua", 101);
+
+        // treino
+        tp.treinarPokemon(p2);
+        tp.treinarPokemon(p2);
+        tp.treinarPokemon(p2);
+        tp.treinarPokemon(p2);
+        tp.treinarPokemon(p2);
+
+        System.out.println(p2);
+
+        tp.evoluirPokemon(p2, "Wartortle");
+
+        System.out.println(p2);
+
+        tp.treinarPokemon(p1);
+        tp.treinarPokemon(p1);
+
+        System.out.println(p1);
+
+        tp.evoluirPokemon(p1, "Raichu");
+        
+        System.out.println(tp);
+    }
+
+}
